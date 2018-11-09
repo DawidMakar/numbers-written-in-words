d651fa1a37de9f4f4723b2fa28f82240e4a415f3
Dawid M <degimek@gmail.com>
2018-11-09 16:44:09 +0100

    NumberToWordsView update

--- zad/core/views.py
+++ zad/core/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
 
@@ -10,11 +9,16 @@ class NumberToWordsView(FormView):
 
     form_class = NumberToWordsForm
     template_name = 'number_to_words.html'
+    result = ''
 
     def form_valid(self, form):
         number = form.cleaned_data['number']
-        result = convert_number_to_words(number)
-        return HttpResponseRedirect(f"{reverse_lazy('result')}?res={result}")
+        self.result = convert_number_to_words(number)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        self.success_url = f"{reverse_lazy('result')}?res={self.result}"
+        return super().get_success_url()
 
 
 class ResultView(TemplateView):
